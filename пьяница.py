2445fba35d8436f6ec32c99a692ffe5e2d2554cb
ksuha <ksusha.rabota4@gmail.com>
2026-05-14 12:08:55 +0300

    Update пьяница.py

--- пьяница.py
+++ пьяница.py
@@ -1,11 +1,27 @@
 import pygame
 import random
 
-width = 460
-height = 360
+background_color = (255, 229, 236)
+
+width = 860
+height = 600
 fps = 30
 
 pygame.init()
-screen = pygame.dis
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("My game")
+clock = pygame.time.Clock()
 
 
+running = True
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(background_color)
+
+    pygame.display.flip()
+    clock.tick(fps)
